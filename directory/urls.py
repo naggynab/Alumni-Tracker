86cb78fe4173357d696a9e58495cea990478d759
laxmi277 <userlaxmi123@gmail.com>
2026-08-29 20:04:16 +0545

    Improve alumni registration and directory search

--- directory/urls.py
+++ directory/urls.py
@@ -9,6 +9,11 @@ app_name = "directory"
 urlpatterns = [
     path("", views.home, name="home"),
     path("alumni/", views.alumni_list, name="alumni-list"),
+    path(
+        "alumni/filter-options/",
+        views.alumni_filter_options,
+        name="alumni-filter-options",
+    ),
     path("alumni/<int:pk>/", views.alumnus_detail, name="alumnus-detail"),
     path("me/", views.my_profile, name="my-profile"),
     path("me/completeness/", views.profile_completeness_page, name="profile-completeness"),
